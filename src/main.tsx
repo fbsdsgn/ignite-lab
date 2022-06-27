78c5750ef3637b864e9d1ebd709d6a880bd50ed5
Felipe B Souza <fbsdsgn@gmail.com>
2022-06-27 13:46:51 -0300

    app

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+
+import "./styles/global.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
